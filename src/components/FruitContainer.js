b434e80aae48218cf036acdbe63daf6f53689b43
jwright1992 <jordanwright1992@gmail.com>
2021-05-27 10:13:06 -0700

    Update

--- src/components/FruitContainer.js
+++ src/components/FruitContainer.js
@@ -1,41 +1,42 @@
-import React, { Component } from "react";
-import List from "./List";
-import Input from "./Input";
+import React, { Component } from 'react';
+import List from './List';
+import Input from './Input';
 
 class FruitContainer extends Component {
-  state = {
-    fruitsToDisplay: this.props.fruits,
-    fruitValue: "",
-  };
-  handleFilterChange = (e) => {
-    e.preventDefault();
-    let value = e.target.value; // test toLowerCase()
+    state = {
+        fruitsToDisplay: this.props.fruits,
+        fruitValue: ''
+    }
+    handleFilterChange = (e) => {
+        e.preventDefault();
+        let value = e.target.value; // test toLowerCase()
 
-    const filteredFruitList = this.props.fruits.filter((fruit) => {
-      // an
-      // we want to check to see if filterValue is included within the fruit
-      if (fruit.includes(value.toLowerCase())) {
-        return true;
-      }
-    });
+        const filteredFruitList = this.props.fruits.filter(fruit => { // an
+            // we want to check to see if filterValue is included within the fruit
+            // if (fruit.includes(value.toLowerCase())) { 
+            //     return true;
+            // } else {
+            //     return false;
+            // }
+            return fruit.includes(value.toLowerCase());
+        })
 
-    this.setState({
-      fruitsToDisplay: filteredFruitList,
-      fruitValue: value,
-    });
-  };
+        // const filteredFruitList = this.props.fruits.filter(fruit => fruit.includes(value.toLowerCase()));
 
-  render() {
-    return (
-      <div>
-        <Input
-          fruitInput={this.state.fruitValue}
-          filter={this.handleFilterChange}
-        />
-        <List fruits={this.state.fruitsToDisplay} />
-      </div>
-    );
-  }
+        this.setState({
+            fruitsToDisplay: filteredFruitList,
+            fruitValue: value
+        })
+    }
+
+    render() {
+        return (
+            <div>
+                <Input fruitInput={this.state.fruitValue} filter={this.handleFilterChange}/>
+                <List fruits={this.state.fruitsToDisplay}/>
+            </div>
+        )
+    }
 }
 
 export default FruitContainer;
